a3626646379f1ed349f9d5ff1d5ef0d5b5bb2d40
Tori Enyart <torienyart@gmail.com>
2023-01-12 18:49:30 -0700

    test: Write tests for braille dictionary

--- spec/english_converter_spec.rb
+++ spec/english_converter_spec.rb
@@ -6,5 +6,22 @@ describe EnglishConverter do
   it 'can exist' do
     expect(converter).to be_an_instance_of(EnglishConverter)
   end
+
+  it 'can have a braille dictionary' do
+    expect(converter.dictionary_hash.count).to eq(26)
+    expect(converter.dictionary_hash.keys).to eq(('a'..'z').to_a)
+    expect(converter.dictionary_hash['a']).to eq(["0.", "..", ".."])
+  end
+
+  it 'stores a braille dictionary' do
+    expect(converter.braille_dictionary).to eq(dictionary_hash))
+
+  end
+  it 'has dictionary keys' do
+    expect(converter.a_key).to eq('00')
+    expect(converter.b_key).to eq('0.')
+    expect(converter.c_key).to eq('.0')
+    expect(converter.d_key).to eq('..')
+  end
   
 end
