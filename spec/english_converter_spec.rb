bcdd00e83e51c16ae5f952ff8fbc744cedf06fff
Tori Enyart <torienyart@gmail.com>
2023-01-12 18:57:05 -0700

    fix: Reorder tests

--- spec/english_converter_spec.rb
+++ spec/english_converter_spec.rb
@@ -7,6 +7,13 @@ describe EnglishConverter do
     expect(converter).to be_an_instance_of(EnglishConverter)
   end
 
+  it 'has dictionary keys' do
+    expect(converter.a_key).to eq('00')
+    expect(converter.b_key).to eq('0.')
+    expect(converter.c_key).to eq('.0')
+    expect(converter.d_key).to eq('..')
+  end
+
   it 'can have a braille dictionary' do
     expect(converter.dictionary_hash.count).to eq(26)
     expect(converter.dictionary_hash.keys).to eq(('a'..'z').to_a)
@@ -15,13 +22,6 @@ describe EnglishConverter do
 
   it 'stores a braille dictionary' do
     expect(converter.braille_dictionary).to eq(dictionary_hash))
-
-  end
-  it 'has dictionary keys' do
-    expect(converter.a_key).to eq('00')
-    expect(converter.b_key).to eq('0.')
-    expect(converter.c_key).to eq('.0')
-    expect(converter.d_key).to eq('..')
   end
   
 end
