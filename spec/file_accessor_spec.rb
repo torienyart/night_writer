88e1df41215e2b87e8eb9e0f1ab0fe347c6374f6
Tori Enyart <torienyart@gmail.com>
2023-01-12 19:19:32 -0700

    test: Write test for correctly formatted english to braille conversion

--- spec/file_accessor_spec.rb
+++ spec/file_accessor_spec.rb
@@ -1,15 +1,22 @@
 require './lib/file_accessor'
 
 describe FileAccessor do
-  it 'can count characters in a file' do
+  xit 'can count characters in a file' do
     allow(FileAccessor).to receive(:message_receiver).and_return(File.open('message.txt', "r"))
     
     expect(FileAccessor.input_character_count).to eq(43)
   end
 
-  it 'can count output characters' do
+  xit 'can count output characters' do
     allow(FileAccessor).to receive(:generate_file).and_return(File.new('braille.txt', "w+"))
 
     expect(FileAccessor.output_character_count).to eq(0)
+  end
+
+  it 'can convert english characters to braille characters' do
+    FileAccessor.convert_to_braille
+    braille = File.open('braille.txt', "r")
+
+    expect(braille.read).to eq('0.\n'+'..\n' + '..')
   end
 end
