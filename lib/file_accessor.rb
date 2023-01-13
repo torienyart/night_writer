7c1b2f32c3231cb6ee189d0f242e480053fbdfd4
Tori Enyart <torienyart@gmail.com>
2023-01-12 18:27:20 -0700

    feat: Create methods to generate new file and count it's characters

--- lib/file_accessor.rb
+++ lib/file_accessor.rb
@@ -4,7 +4,15 @@ class FileAccessor
     File.open(ARGV[0], "r")
   end
 
-  def self.character_count
+  def self.input_character_count
     message_receiver.read.size
+  end
+
+  def self.generate_file
+    File.new(ARGV[1], "w+")
+  end
+
+  def self.output_character_count
+    generate_file.read.size
   end
 end
