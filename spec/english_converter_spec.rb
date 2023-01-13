c6bd3f226217a48b5988e05c544a8982a369ee78
Tori Enyart <torienyart@gmail.com>
2023-01-12 18:33:46 -0700

    test: Write test to initialize EnglishConverter

--- spec/english_converter_spec.rb
+++ spec/english_converter_spec.rb
@@ -1,5 +1,10 @@
 require './lib/english_converter'
 
 describe EnglishConverter do
+  let(:converter){EnglishConverter.new}
+
+  it 'can exist' do
+    expect(converter).to be_an_instance_of(EnglishConverter)
+  end
   
 end
