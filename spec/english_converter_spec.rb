6d6a7b4852fd027980fb6d3f547b52fc336b8e6f
Tori Enyart <torienyart@gmail.com>
2023-01-15 14:18:17 -0700

    fix: Adjust converter test to include more letters

--- spec/english_converter_spec.rb
+++ spec/english_converter_spec.rb
@@ -29,40 +29,41 @@ describe EnglishConverter do
     expect(converter.braille_dictionary).to eq(converter.dictionary_hash)
   end
 
-  it 'can convert english characters to braille characters' do
-    allow(FileAccessor).to receive(:generate_file).and_return(File.new('braille.txt', "w+"))
-    allow(converter).to receive(:english_characters).and_return(["a"])
-
-    converter.convert_to_braille
-    
-    braille = File.open('braille.txt', "r")
-
-    expect(braille.read).to eq("0.\n"+"..\n" + "..")
-  end
-
+  
   it 'can pull english characters into array' do
     allow(FileAccessor).to receive(:generate_file).and_return(File.new('braille.txt', "w+"))
     allow(FileAccessor).to receive(:message_receiver).and_return(File.open('message.txt', "r"))
-
+    
     expect(converter.english_characters).to eq(["a", "b", "c"])
   end
-
+  
   it 'can replace english characters w/ braille' do
     allow(converter).to receive(:english_characters).and_return(["a"])
-
+    
     expect(converter.replace_characters).to eq(["0.", "..", ".."])
   end
-
+  
   it 'can format braille characters' do
     allow(converter).to receive(:english_characters).and_return(["a"])
-
+    
     expect(converter.format_braille).to eq("0.\n"+"..\n" + "..")
   end
+  
+  # it 'can take and format multiple letters' do
+  #   allow(converter).to receive(:english_characters).and_return(["a", "b", "c"])
+  
+  #   expect(converter.format_braille).to eq("0.0.00\n"+"..0...\n" + "......")
+  # end
 
-  it 'can take and format multiple letters' do
+  it 'can convert english characters to braille characters' do
+    allow(FileAccessor).to receive(:generate_file).and_return(File.new('braille.txt', "w+"))
     allow(converter).to receive(:english_characters).and_return(["a", "b", "c"])
-
-    expect(converter.format_braille).to eq("0.0.00\n"+"..0...\n" + "......")
+    
+    converter.convert_to_braille
+    
+    braille = File.open('braille.txt', "r")
+    
+    expect(braille.read).to eq("0.0.00\n"+"..0...\n" + "......")
   end
   
 end
