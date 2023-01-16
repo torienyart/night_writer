f70c29f0f56a45ae46e89dee55eccba3222cca4b
Tori Enyart <torienyart@gmail.com>
2023-01-15 18:47:40 -0700

    doc: Added remaining code to run program properly

--- lib/night_reader.rb
+++ lib/night_reader.rb
@@ -2,9 +2,9 @@ require_relative 'file_accessor'
 require_relative 'braille_converter'
 
 
-# new_conversion = EnglishConverter.new
+new_conversion = BrailleConverter.new
 
-# new_conversion.convert_to_braille
+new_conversion.convert_to_english
 
-# message_characters = FileAccessor.output_character_count
-# p "Created '#{ARGV[1]}' containing #{message_characters} characters"
+message_characters = FileAccessor.output_character_count
+p "Created '#{ARGV[1]}' containing #{message_characters} characters"
