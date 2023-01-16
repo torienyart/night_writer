68091c5fb7fbfea0bcba08c2ccad2140c553af7c
Tori Enyart <torienyart@gmail.com>
2023-01-15 17:11:22 -0700

    doc: Set up night reader w/ info from night writer

---
+++ lib/night_reader.rb
@@ -0,0 +1,10 @@
+require_relative 'file_accessor'
+require_relative 'braille_converter'
+
+
+# new_conversion = EnglishConverter.new
+
+# new_conversion.convert_to_braille
+
+# message_characters = FileAccessor.output_character_count
+# p "Created '#{ARGV[1]}' containing #{message_characters} characters"
