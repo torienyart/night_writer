23a8c93e21642c6dbf35124840aaa8bdeb2bd939
Tori Enyart <torienyart@gmail.com>
2023-01-15 19:16:35 -0700

    fix: Update tests to use dummy files

--- spec/file_accessor_spec.rb
+++ spec/file_accessor_spec.rb
@@ -2,14 +2,14 @@ require './lib/file_accessor'
 
 describe FileAccessor do
   it 'can count characters in a file' do
-    allow(FileAccessor).to receive(:message_receiver).and_return(File.open('message.txt', "r"))
+    allow(FileAccessor).to receive(:message_receiver).and_return(File.open('dummy_message.txt', "r"))
     
     expect(FileAccessor.input_character_count).to eq(3)
   end
 
   it 'can count output characters' do
-    allow(FileAccessor).to receive(:open_new_file).and_return(File.open('braille.txt', "r"))
+    allow(FileAccessor).to receive(:open_new_file).and_return(File.open('dummy_braille.txt', "r"))
 
-    expect(FileAccessor.output_character_count).to eq(0)
+    expect(FileAccessor.output_character_count).to eq(20)
   end
 end
